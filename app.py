b5526ab1bde945fc22ba856d1cfacb08a2a86f83
Jakub Krol <jakub.krol@poczta.fm>
2018-04-22 16:13:36 +0200

    Some kind of getting songs

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 import base64
 import urllib
 import yaml
-from flask import Flask, request, redirect, g, render_template
+from flask import Flask, request, redirect, g, render_template, Response
 import spotipy
 
 conf = yaml.load(open('conf/application.yml'))
@@ -22,7 +22,7 @@ SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
 CLIENT_SIDE_URL = "http://127.0.0.1"
 PORT = 8080
 REDIRECT_URI = "{}:{}/callback/q".format(CLIENT_SIDE_URL, PORT)
-SCOPE = "playlist-modify-public playlist-modify-private"
+SCOPE = "playlist-modify-public playlist-modify-private user-top-read user-read-recently-played user-library-read user-follow-read"
 STATE = ""
 SHOW_DIALOG_bool = True
 SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
@@ -45,11 +45,24 @@ def login():
     auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
     return redirect(auth_url)
 
-@app.route("/moodify")
-def get_playlist(): 
-    req =  requests.get("https://api.spotify.com/v1/me")
-    return req.text 
- 
+def get_songs_from_saved_tracks(songs, authorization_header):
+    for offset in xrange(100):
+        req = requests.get('https://api.spotify.com/v1/me/tracks?offset='+str(50*offset)+'&limit=50', headers=authorization_header)
+        if req.json()['next'] == None:
+            print(offset)
+            break
+        for track in req.json()['items']:
+            songs.append(track['track']['external_urls']['spotify'])
+
+
+def get_songs_from_saved_artists(songs, authorization_header):
+    req =  requests.get("https://api.spotify.com/v1/me/following?type=artist", headers=authorization_header)
+    artists_ids = [a_id['id'] for a_id in req.json()['artists']['items']]
+    for artist in artists_ids:
+        res = requests.get("https://api.spotify.com/v1/artists/"+artist+"/top-tracks?country=PL&limit=50", headers=authorization_header)
+        for track in res.json()['tracks']:
+            songs.append(track['external_urls']['spotify'])
+     
 @app.route("/callback/q")
 def callback():
     auth_token = request.args['code']
@@ -69,10 +82,12 @@ def callback():
     expires_in = response_data["expires_in"]
 
     authorization_header = {"Authorization":"Bearer {}".format(access_token)}
+    songs = []
     
-    req =  requests.get("https://api.spotify.com/v1/me", headers=authorization_header)
-    return req.text 
+    get_songs_from_saved_tracks(songs, authorization_header)
+    get_songs_from_saved_artists(songs, authorization_header)
+    print(len(songs))
+    return str(songs) 
     return render_template('moodify.html')
-#    return redirect("/moodify")
 
 app.run(host='0.0.0.0',port=PORT)
